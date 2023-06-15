a30a451cfd6afed9813d7fc0c743d6afcaaf88ad
Andrievich Regina <andrievichregina@Mac-mini-Andrievich.local>
2023-06-15 12:58:16 +1000

    changing the way styles are included

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 // правильный способ подключить стили и скрипты
 add_action('wp_enqueue_scripts', function () {
     
-    wp_enqueue_style('style-main', get_template_directory_uri() . '/assets/css/style.min.css');
+    wp_enqueue_style('style-main', get_template_directory_uri() . '/assets/css/style.min.css?'.time(), array(), null );
 
     //wp_deregister_script('jquery');
     // wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
